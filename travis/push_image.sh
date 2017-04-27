e5200ac329faaac2428dfe0311aadbea19f902a0
ascrutae <nzt48.xin@gmail.com>
2017-04-27 19:03:29 +0800

    no message

--- travis/push_image.sh
+++ travis/push_image.sh
@@ -5,7 +5,7 @@ push_image() {
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
   mvn clean package docker:build
   docker push skywalking/sky-walking-ui:latest
-  docker push skywalking/sky-walking-ui:3.0.1
+  docker push skywalking/sky-walking-ui:3.0.1-2017
 }
 
 
